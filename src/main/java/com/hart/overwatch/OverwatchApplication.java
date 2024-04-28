98ee88a86dd6c60f48e18a0146e60533cb16dbe7
Ian Hart <ianhart7790@gmail.com>
2024-04-28 12:12:20 -0400

    feat: initial commit

---
+++ src/main/java/com/hart/overwatch/OverwatchApplication.java
@@ -0,0 +1,13 @@
+package com.hart.overwatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OverwatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OverwatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hart/overwatch/OverwatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hart.overwatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OverwatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
