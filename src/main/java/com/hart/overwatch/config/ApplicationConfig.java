edd0ae1483c32fa8fd9d55fe6f5c3a3a1d299de2
Ian Hart <ianhart7790@gmail.com>
2024-04-28 14:10:21 -0400

    feat: add get mapping for heartbeat

--- src/main/java/com/hart/overwatch/config/ApplicationConfig.java
+++ src/main/java/com/hart/overwatch/config/ApplicationConfig.java
@@ -61,8 +61,8 @@ public class ApplicationConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(
-                Arrays.asList("https://localhost:8080", "http://localhost:5173"));
+        configuration
+                .setAllowedOrigins(Arrays.asList("http://localhost:8080", "http://localhost:5173"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PATCH", "UPDATE", "DELETE"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
--- src/main/java/com/hart/overwatch/heartbeat/HeartBeatController.java
+++ src/main/java/com/hart/overwatch/heartbeat/HeartBeatController.java
@@ -2,6 +2,7 @@ package com.hart.overwatch.heartbeat;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.hart.overwatch.heartbeat.response.HeartBeatResponse;
@@ -10,6 +11,7 @@ import com.hart.overwatch.heartbeat.response.HeartBeatResponse;
 @RequestMapping(path = "/api/v1/heartbeat")
 public class HeartBeatController {
 
+    @GetMapping("")
     public ResponseEntity<HeartBeatResponse> getHeartBeat() {
         return ResponseEntity.status(HttpStatus.OK).body(new HeartBeatResponse("success"));
     }
