0c99049d2f189a8b52b60dad2c542dfea3e05a15
Ian Hart <ianhart7790@gmail.com>
2024-04-30 18:40:21 -0400

    feat: implement a responsive hamburger menu

--- frontend/src/components/Navbar/NavbarLink.tsx
+++ frontend/src/components/Navbar/NavbarLink.tsx
@@ -3,11 +3,12 @@ import { NavLink } from 'react-router-dom';
 interface INavbarLinkProps {
   path: string;
   title: string;
+  showLoggedIn: boolean;
 }
 
-const NavbarLink = ({ path, title }: INavbarLinkProps) => {
+const NavbarLink = ({ path, title, showLoggedIn }: INavbarLinkProps) => {
   return (
-    <li className="md:mx-2">
+    <li className="md:mx-2 md:p-0 p-2 hover:bg-gray-800 transition ease-in-out rounded">
       <NavLink to={path}>{title}</NavLink>
     </li>
   );
---
+++ frontend/src/state/navbar/navbarSlice.ts
@@ -0,0 +1,26 @@
+import { PayloadAction, createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+interface INavbarState {
+  isMobileOpen: boolean;
+}
+
+const initialState: INavbarState = {
+  isMobileOpen: false,
+};
+
+const navbarSlice = createSlice({
+  name: 'navbar',
+  initialState,
+  reducers: {
+    openMobile: (state) => {
+      state.isMobileOpen = true;
+    },
+    closeMobile: (state) => {
+      state.isMobileOpen = false;
+    },
+  },
+});
+
+export const { openMobile, closeMobile } = navbarSlice.actions;
+
+export default navbarSlice.reducer;
