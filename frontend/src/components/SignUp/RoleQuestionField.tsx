664fd2848b0789b252d4e6bc6aee6e61fd20f43b
Ian Hart <ianhart7790@gmail.com>
2024-05-01 14:20:45 -0400

    feat: add question to role question field

--- frontend/src/components/SignUp/RoleQuestionField.tsx
+++ frontend/src/components/SignUp/RoleQuestionField.tsx
@@ -3,6 +3,7 @@ import RoleAnswer from './RoleAnswer';
 const RoleQuestionField = () => {
   return (
     <div>
+      <p>What best describes you?</p>
       <RoleAnswer role={Role.USER} answer="I am looking for someone to review my code" />
       <RoleAnswer role={Role.REVIEWER} answer="I am looking to review the code of others" />
     </div>
