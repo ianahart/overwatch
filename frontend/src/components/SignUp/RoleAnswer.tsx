b1333e8bd32965fd0df319055f7604eaf8b5f756
Ian Hart <ianhart7790@gmail.com>
2024-05-05 11:18:58 -0400

    refactor: refactor role question so it only shows the answer that was selected

--- frontend/src/components/SignUp/RoleAnswer.tsx
+++ frontend/src/components/SignUp/RoleAnswer.tsx
@@ -13,21 +13,25 @@ const RoleAnswer = ({ answer, role }: IRoleAnswerProps) => {
   const { role: roleState } = useSelector((store: TRootState) => store.signup);
   const dispatch = useDispatch();
   return (
-    <div>
-      <div
-        onClick={() => dispatch(updateRole(role))}
-        className={`my-2 ${
-          roleState.value === role ? 'bg-green-400 text-black animate-bounce-short' : 'bg-slate-800 text-inherit'
-        } p-2 rounded cursor-pointer`}
-      >
-        {roleState.value === role && (
-          <div className="text-green-700 text-2xl">
-            <AiOutlineCheck />
+    <>
+      {(roleState.value === Role.UNASSIGNED || roleState.value === role) && (
+        <div>
+          <div
+            onClick={() => dispatch(updateRole(role === roleState.value ? Role.UNASSIGNED : role))}
+            className={`my-2 ${
+              roleState.value === role ? 'bg-green-400 text-black animate-bounce-short' : 'bg-inherit text-inherit'
+            } p-2 rounded cursor-pointer`}
+          >
+            {roleState.value === role && (
+              <div className="text-green-700 text-2xl">
+                <AiOutlineCheck />
+              </div>
+            )}
+            <p>{answer}</p>
           </div>
-        )}
-        <p>{answer}</p>
-      </div>
-    </div>
+        </div>
+      )}
+    </>
   );
 };
 
--- frontend/src/components/SignUp/RoleQuestionField.tsx
+++ frontend/src/components/SignUp/RoleQuestionField.tsx
@@ -1,9 +1,14 @@
 import { Role } from '../../enums';
 import RoleAnswer from './RoleAnswer';
+import { PiBaseballCap } from 'react-icons/pi';
+
 const RoleQuestionField = () => {
   return (
-    <div>
-      <p>What best describes you?</p>
+    <div className="mt-2">
+      <div className="flex items-center">
+        <PiBaseballCap className="mr-2" />
+        <p className="text-gray-400">What best describes you?</p>
+      </div>
       <RoleAnswer role={Role.USER} answer="I am looking for someone to review my code" />
       <RoleAnswer role={Role.REVIEWER} answer="I am looking to review the code of others" />
     </div>
