0ecdf704e9302184fad3868ce714beb27686c0d7
Ian Hart <ianhart7790@gmail.com>
2024-05-05 11:37:32 -0400

    feat: add validation to form input field component

--- frontend/src/components/Form/FormInputField.tsx
+++ frontend/src/components/Form/FormInputField.tsx
@@ -9,6 +9,8 @@ export interface IFormInputFieldProps {
   id: string;
   errorField: string;
   placeholder: string;
+  min?: number;
+  max?: number;
 }
 
 const FormInputField = ({
@@ -22,10 +24,25 @@ const FormInputField = ({
   id,
   errorField,
   placeholder,
+  min = 1,
+  max = 200,
 }: IFormInputFieldProps) => {
   const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { value, name } = e.target;
     handleUpdateField(name, value, 'value');
+  };
+
+  const handleOnBlur = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value, name } = e.target;
+    if (value.length < min || value.length > max) {
+      const validationError = `${errorField} must be between ${min} and ${max} characters`;
+      handleUpdateField(name, validationError, 'error');
+    }
+  };
+
+  const handleOnFocus = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name } = e.target;
+    handleUpdateField(name, '', 'error');
   };
 
   return (
@@ -36,15 +53,18 @@ const FormInputField = ({
       <div className="relative">
         <input
           onChange={handleOnChange}
-          className="h-9 rounded bg-slate-800 w-full placeholder:pl-6 pl-6 shadow"
+          onBlur={handleOnBlur}
+          onFocus={handleOnFocus}
+          className="h-9 rounded bg-slate-800 w-full placeholder:pl-6 pl-8 shadow"
           id={id}
           name={name}
           value={value}
           placeholder={placeholder}
           type={type}
         />
-        <div className="absolute top-2">{icon}</div>
+        <div className="absolute top-2 left-2">{icon}</div>
       </div>
+      {error.length > 0 && <p className="text-red-400 mt-1 text-sm">{error}</p>}
     </div>
   );
 };
--- frontend/src/components/SignUp/Form.tsx
+++ frontend/src/components/SignUp/Form.tsx
@@ -4,19 +4,19 @@ import { TRootState } from '../../state/store';
 import FormInputField from '../Form/FormInputField';
 import { AiOutlineUser } from 'react-icons/ai';
 import { updateField } from '../../state/store';
+import { FaRegEnvelope } from 'react-icons/fa';
 
 const Form = () => {
   const form = useSelector((store: TRootState) => store.signup);
   const dispatch = useDispatch();
 
   const handleUpdateField = (name: string, value: string, attribute: string) => {
-    console.log(name, value, attribute);
     dispatch(updateField({ name, value, attribute }));
   };
 
   return (
     <div className="bg-slate-900 lg:w-3/6 rounded-r p-4">
-      <form className="border max-w-xl mx-auto">
+      <form className="max-w-xl mx-auto">
         <header className="text-center">
           <h1 className="text-2xl font-display text-green-400">Sign up for OverWatch</h1>
           <p>Create a free account or log in</p>
@@ -49,6 +49,20 @@ const Form = () => {
               errorField="Last name"
               placeholder="Enter your last name"
               icon={<AiOutlineUser />}
+            />
+          </div>
+          <div className="my-4">
+            <FormInputField
+              handleUpdateField={handleUpdateField}
+              name={form.email.name}
+              value={form.email.value}
+              error={form.email.error}
+              type={form.email.type}
+              label="Email"
+              id="email"
+              errorField="Email"
+              placeholder="Enter your email"
+              icon={<FaRegEnvelope />}
             />
           </div>
         </section>
