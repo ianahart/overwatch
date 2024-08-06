106c760911a98f1e2c920e63d109713b91772c45
Ian Hart <ianhart7790@gmail.com>
2024-08-06 12:50:37 -0400

    test: add a service test for refresh token for generating a refresh token

---
+++ src/test/java/com/hart/overwatch/refreshtoken/RefreshTokenServiceTest.java
@@ -0,0 +1,81 @@
+package com.hart.overwatch.refreshtoken;
+
+import org.mockito.ArgumentCaptor;
+import static org.mockito.ArgumentCaptor.forClass;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Arrays;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import com.hart.overwatch.profile.Profile;
+import com.hart.overwatch.setting.Setting;
+import com.hart.overwatch.user.Role;
+import com.hart.overwatch.user.User;
+import com.hart.overwatch.user.UserRepository;
+import com.hart.overwatch.user.UserService;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@ExtendWith(MockitoExtension.class)
+public class RefreshTokenServiceTest {
+
+    @InjectMocks
+    private RefreshTokenService refreshTokenService;
+
+    @Mock
+    private RefreshTokenRepository refreshTokenRepository;
+
+    @Mock
+    private UserRepository userRepository;
+
+    private User user;
+
+    List<RefreshToken> refreshTokens = new ArrayList<>();
+
+    @BeforeEach
+    void setUp() {
+        boolean loggedIn = true;
+        user = new User("john@mail.com", "John", "Doe", "John Doe", Role.USER, loggedIn,
+                new Profile(), "Test12345%", new Setting());
+
+        user.setId(1L);
+
+        int numOfTokensToGenerate = 2;
+
+        for (int i = 0; i < numOfTokensToGenerate; i++) {
+            String tokenValue = "token" + UUID.randomUUID().toString();
+            refreshTokens.add(new RefreshToken(Long.valueOf(i + 1), tokenValue,
+                    Instant.now().plusMillis(86400000 * 12), user));
+        }
+        user.setRefreshTokens(refreshTokens);
+    }
+
+    @Test
+    public void RefreshTokenService_GenerateRefreshToken_ReturnRefreshToken() {
+        String tokenValue = "token" + UUID.randomUUID().toString();
+        RefreshToken refreshTokenToSave =
+                new RefreshToken(3L, tokenValue, Instant.now().plusMillis(86400000 * 12), user);
+
+        when(userRepository.findById(user.getId())).thenReturn(Optional.ofNullable(user));
+        when(refreshTokenRepository.save(any(RefreshToken.class))).thenReturn(refreshTokenToSave);
+
+        RefreshToken returnedRefreshToken = refreshTokenService.generateRefreshToken(user.getId());
+
+        Assertions.assertThat(returnedRefreshToken).isNotNull();
+        verify(refreshTokenRepository, times(1)).save(any(RefreshToken.class));
+    }
+
+}
+
+
