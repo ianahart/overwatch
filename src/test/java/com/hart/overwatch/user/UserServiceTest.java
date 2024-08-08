480bae4ae88312ce9dd6cea01b62049abd79ff2f
Ian Hart <ianhart7790@gmail.com>
2024-08-08 16:25:30 -0400

    test: add tests for the user service class

--- src/test/java/com/hart/overwatch/user/UserServiceTest.java
+++ src/test/java/com/hart/overwatch/user/UserServiceTest.java
@@ -7,8 +7,14 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 import java.util.List;
 import java.util.Optional;
+
+import javax.crypto.SecretKey;
+
+import java.security.Key;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Base64;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -17,20 +23,37 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.hart.overwatch.advice.ForbiddenException;
 import com.hart.overwatch.advice.NotFoundException;
 import com.hart.overwatch.profile.Profile;
 import com.hart.overwatch.setting.Setting;
+import com.hart.overwatch.user.dto.UpdateUserDto;
+import com.hart.overwatch.user.dto.UserDto;
+import com.hart.overwatch.user.request.UpdateUserRequest;
+import com.twilio.twiml.voice.Prompt.For;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 
 @ActiveProfiles("test")
+@TestPropertySource(locations = "classpath:application-test.properties")
 @ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
+
+    @Value("${secretkey}")
+    private String secretKey;
 
     @InjectMocks
     private UserService userService;
@@ -46,10 +69,16 @@ public class UserServiceTest {
     @BeforeEach
     void setUp() {
         Boolean loggedIn = true;
+        Profile profile = new Profile();
+        Setting setting = new Setting();
+        setting.setId(1L);
+        profile.setId(1L);
+        profile.setAvatarUrl("http://example.com/avatar.jpg");
         user = new User("john@mail.com", "John", "Doe", "John Doe", Role.USER, loggedIn,
-                new Profile(), "Test12345%", new Setting());
+                profile, "Test12345%", setting);
 
         user.setId(1L);
+
     }
 
     @Test
@@ -120,4 +149,198 @@ public class UserServiceTest {
         Assertions.assertThat(returnedUser.getEmail()).isEqualTo(user.getEmail());
     }
 
+    @Test
+    public void UserService_UpdateUserPassword_ReturnNothing() {
+        SecurityContext securityContext = mock(SecurityContext.class);
+        Authentication authentication = mock(Authentication.class);
+        UserDetails userDetails = mock(UserDetails.class);
+        String username = user.getEmail();
+
+        when(securityContext.getAuthentication()).thenReturn(authentication);
+        when(authentication.getPrincipal()).thenReturn(userDetails);
+        when(userDetails.getUsername()).thenReturn(username);
+
+        SecurityContextHolder.setContext(securityContext);
+
+        String currentEncodedPassword = "encodedCurrentPassword";
+        String newEncodedPassword = "encodedNewPassword";
+
+        user.setPassword(currentEncodedPassword);
+
+        when(userRepository.findByEmail(user.getEmail())).thenReturn(Optional.of(user));
+        when(passwordEncoder.matches("Test12345%", "encodedCurrentPassword")).thenReturn(true);
+        when(passwordEncoder.matches("Test12345%%", "encodedCurrentPassword")).thenReturn(false);
+        when(passwordEncoder.encode("Test12345%%")).thenReturn(newEncodedPassword);
+
+        userService.updateUserPassword("Test12345%", "Test12345%%", user.getId());
+
+        verify(userRepository).save(user);
+        Assertions.assertThat(user.getPassword()).isEqualTo(newEncodedPassword);
+    }
+
+    @Test
+    public void UserService_UpdateUserPasswordAnotherUser_ThrowForbiddenException() {
+        SecurityContext securityContext = mock(SecurityContext.class);
+        Authentication authentication = mock(Authentication.class);
+        UserDetails userDetails = mock(UserDetails.class);
+        String username = user.getEmail();
+
+        when(securityContext.getAuthentication()).thenReturn(authentication);
+        when(authentication.getPrincipal()).thenReturn(userDetails);
+        when(userDetails.getUsername()).thenReturn(username);
+
+        SecurityContextHolder.setContext(securityContext);
+
+        String currentEncodedPassword = "encodedCurrentPassword";
+
+        user.setPassword(currentEncodedPassword);
+
+        when(userRepository.findByEmail(user.getEmail())).thenReturn(Optional.of(user));
+
+        Assertions.assertThatThrownBy(() -> userService.updateUserPassword("Test12345%", "Test12345%%", 2L))
+                .isInstanceOf(ForbiddenException.class);
+
+    }
+
+    @Test
+    public void UserService_UpdateUserPasswordPasswordSame_ThrowForbiddenException() {
+        SecurityContext securityContext = mock(SecurityContext.class);
+        Authentication authentication = mock(Authentication.class);
+        UserDetails userDetails = mock(UserDetails.class);
+        String username = user.getEmail();
+
+        when(securityContext.getAuthentication()).thenReturn(authentication);
+        when(authentication.getPrincipal()).thenReturn(userDetails);
+        when(userDetails.getUsername()).thenReturn(username);
+
+        SecurityContextHolder.setContext(securityContext);
+
+        String currentEncodedPassword = "encodedCurrentPassword";
+
+        user.setPassword(currentEncodedPassword);
+
+        when(userRepository.findByEmail(user.getEmail())).thenReturn(Optional.of(user));
+        when(passwordEncoder.matches("Test12345%", currentEncodedPassword)).thenReturn(true);
+        when(passwordEncoder.matches("Test12345%", currentEncodedPassword)).thenReturn(true);
+
+        Assertions.assertThatThrownBy(() -> userService.updateUserPassword("Test12345%", "Test12345%", 1L))
+                .isInstanceOf(ForbiddenException.class)
+                .hasMessage("Your current password is invalid or is the same as your old one");
+
+    }
+
+    @Test
+    public void UserService_DeleteUser_ThrowForbiddenExceptionWrongUser() {
+        SecurityContext securityContext = mock(SecurityContext.class);
+        Authentication authentication = mock(Authentication.class);
+        UserDetails userDetails = mock(UserDetails.class);
+        String username = user.getEmail();
+
+        when(securityContext.getAuthentication()).thenReturn(authentication);
+        when(authentication.getPrincipal()).thenReturn(userDetails);
+        when(userDetails.getUsername()).thenReturn(username);
+
+        SecurityContextHolder.setContext(securityContext);
+
+        String currentEncodedPassword = "encodedCurrentPassword";
+
+        user.setPassword(currentEncodedPassword);
+
+        when(userRepository.findByEmail(user.getEmail())).thenReturn(Optional.of(user));
+
+        Assertions.assertThatThrownBy(() -> userService.deleteUser(2L, "Test12345%"))
+                .isInstanceOf(ForbiddenException.class).hasMessage("Cannot delete another user");
+    }
+
+    @Test
+    public void UserService_DeleteUser_ThrowForbiddenExceptionWrongPassword() {
+        SecurityContext securityContext = mock(SecurityContext.class);
+        Authentication authentication = mock(Authentication.class);
+        UserDetails userDetails = mock(UserDetails.class);
+        String username = user.getEmail();
+
+        when(securityContext.getAuthentication()).thenReturn(authentication);
+        when(authentication.getPrincipal()).thenReturn(userDetails);
+        when(userDetails.getUsername()).thenReturn(username);
+
+        SecurityContextHolder.setContext(securityContext);
+
+        String currentEncodedPassword = "encodedCurrentPassword";
+
+        user.setPassword(currentEncodedPassword);
+
+        when(userRepository.findByEmail(user.getEmail())).thenReturn(Optional.of(user));
+        when(passwordEncoder.matches("Test12345%", currentEncodedPassword)).thenReturn(false);
+
+        Assertions.assertThatThrownBy(() -> userService.deleteUser(user.getId(), "Test12345%"))
+                .isInstanceOf(ForbiddenException.class).hasMessage("Password is not a match");
+    }
+
+    @Test
+    public void UserService_DeleteUser_DoNothing() {
+        SecurityContext securityContext = mock(SecurityContext.class);
+        Authentication authentication = mock(Authentication.class);
+        UserDetails userDetails = mock(UserDetails.class);
+        String username = user.getEmail();
+
+        when(securityContext.getAuthentication()).thenReturn(authentication);
+        when(authentication.getPrincipal()).thenReturn(userDetails);
+        when(userDetails.getUsername()).thenReturn(username);
+
+        SecurityContextHolder.setContext(securityContext);
+
+        String currentEncodedPassword = "encodedCurrentPassword";
+
+        user.setPassword(currentEncodedPassword);
+
+        when(userRepository.findByEmail(user.getEmail())).thenReturn(Optional.of(user));
+        when(passwordEncoder.matches("Test12345%", currentEncodedPassword)).thenReturn(true);
+        doNothing().when(userRepository).delete(user);
+        userService.deleteUser(user.getId(), "Test12345%");
+
+        verify(userRepository, times(1)).delete(user);
+    }
+
+    @Test
+    public void UserService_UpdateUser_ThrowForbiddenException() {
+        SecurityContext securityContext = mock(SecurityContext.class);
+        Authentication authentication = mock(Authentication.class);
+        UserDetails userDetails = mock(UserDetails.class);
+        String username = user.getEmail();
+
+        when(securityContext.getAuthentication()).thenReturn(authentication);
+        when(authentication.getPrincipal()).thenReturn(userDetails);
+        when(userDetails.getUsername()).thenReturn(username);
+        SecurityContextHolder.setContext(securityContext);
+        when(userRepository.findByEmail(user.getEmail())).thenReturn(Optional.of(user));
+
+        UpdateUserRequest request = new UpdateUserRequest("john", "smith", "smith@gmail.com");
+
+        Assertions.assertThatThrownBy(() -> userService.updateUser(request, 999L))
+                .isInstanceOf(ForbiddenException.class).hasMessage("Cannot update another user's information");
+
+    }
+
+    @Test
+    public void UserService_UpdateUser_ReturnUpdateUserDto() {
+        SecurityContext securityContext = mock(SecurityContext.class);
+        Authentication authentication = mock(Authentication.class);
+        UserDetails userDetails = mock(UserDetails.class);
+        String username = user.getEmail();
+
+        when(securityContext.getAuthentication()).thenReturn(authentication);
+        when(authentication.getPrincipal()).thenReturn(userDetails);
+        when(userDetails.getUsername()).thenReturn(username);
+        SecurityContextHolder.setContext(securityContext);
+        when(userRepository.findByEmail(user.getEmail())).thenReturn(Optional.of(user));
+
+        UpdateUserRequest request = new UpdateUserRequest("john", "smith", "smith@gmail.com");
+
+        UpdateUserDto result = userService.updateUser(request, user.getId());
+
+        Assertions.assertThat(result.getFirstName().toLowerCase()).isEqualTo(result.getFirstName().toLowerCase());
+        Assertions.assertThat(result.getLastName().toLowerCase()).isEqualTo(result.getLastName().toLowerCase());
+        Assertions.assertThat(result.getEmail()).isEqualTo(result.getEmail());
+
+    }
 }
