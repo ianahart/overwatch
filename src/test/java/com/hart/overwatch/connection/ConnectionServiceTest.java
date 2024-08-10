ef7b1c7766412c62310e657d9bda054dc1a91f0b
Ian Hart <ianhart7790@gmail.com>
2024-08-10 16:11:36 -0400

    test: add connection service tests for getting all connections by query and by search

--- src/test/java/com/hart/overwatch/connection/ConnectionServiceTest.java
+++ src/test/java/com/hart/overwatch/connection/ConnectionServiceTest.java
@@ -1,6 +1,7 @@
 package com.hart.overwatch.connection;
 
 import static org.mockito.Mockito.*;
+import java.util.List;
 import java.util.Optional;
 
 import org.assertj.core.api.Assertions;
@@ -11,16 +12,24 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import com.hart.overwatch.advice.BadRequestException;
+import com.hart.overwatch.advice.ForbiddenException;
 import com.hart.overwatch.advice.NotFoundException;
+import com.hart.overwatch.chatmessage.ChatMessage;
+import com.hart.overwatch.connection.dto.ConnectionDto;
 import com.hart.overwatch.connection.dto.MinConnectionDto;
 import com.hart.overwatch.connectionpin.ConnectionPinService;
 import com.hart.overwatch.location.Location;
 import com.hart.overwatch.pagination.PaginationService;
+import com.hart.overwatch.pagination.dto.PaginationDto;
 import com.hart.overwatch.profile.Profile;
 import com.hart.overwatch.setting.Setting;
 import com.hart.overwatch.user.Role;
 import com.hart.overwatch.user.User;
 import com.hart.overwatch.user.UserService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("test")
@@ -49,6 +58,10 @@ public class ConnectionServiceTest {
 
     private User receiver;
 
+    private ConnectionDto senderConnectionDto;
+
+    private ConnectionDto receiverConnectionDto;
+
     private User createSender() {
         Boolean loggedIn = false;
         User sender = new User("john@mail.com", "John", "Doe", "John Doe", Role.USER, loggedIn,
@@ -64,6 +77,7 @@ public class ConnectionServiceTest {
         location.setCountry("United States");
         location.setCity("New York City");
         location.setUser(sender);
+        sender.setLocation(location);
 
         location.setId(1L);
         sender.setId(1L);
@@ -87,6 +101,7 @@ public class ConnectionServiceTest {
         location.setCountry("United States");
         location.setCity("New York City");
         location.setUser(receiver);
+        receiver.setLocation(location);
 
         location.setId(2L);
         receiver.setId(2L);
@@ -102,6 +117,18 @@ public class ConnectionServiceTest {
 
         connection = new Connection(RequestStatus.ACCEPTED, sender, receiver);
         connection.setId(1L);
+
+        receiverConnectionDto = new ConnectionDto(connection.getId(), receiver.getId(),
+                sender.getId(), sender.getFirstName(), sender.getLastName(),
+                sender.getProfile().getAvatarUrl(), sender.getProfile().getEmail(),
+                sender.getLocation().getCity(), sender.getLocation().getCountry(),
+                sender.getProfile().getContactNumber(), sender.getProfile().getBio());
+
+        senderConnectionDto = new ConnectionDto(connection.getId(), receiver.getId(),
+                sender.getId(), receiver.getFirstName(), receiver.getLastName(),
+                receiver.getProfile().getAvatarUrl(), receiver.getProfile().getEmail(),
+                receiver.getLocation().getCity(), receiver.getLocation().getCountry(),
+                receiver.getProfile().getContactNumber(), receiver.getProfile().getBio());
     }
 
     @Test
@@ -184,6 +211,201 @@ public class ConnectionServiceTest {
       Assertions.assertThat(minConnectionDto.getId()).isEqualTo(connection.getId());
     }
 
+    @Test
+    public void ConnectionService_GetAllConnections_ThrowForbiddenException() {
+        when(userService.getUserById(sender.getId())).thenReturn(sender);
+        when(userService.getCurrentlyLoggedInUser()).thenReturn(receiver);
 
+        Assertions.assertThatThrownBy(() -> {
+            connectionService.getAllConnections(sender.getId(), 0, 10, "next", "false");
+        }).isInstanceOf(ForbiddenException.class).hasMessage("Cannot load another user's connections");
+    }
+
+    @Test
+    public void ConnectionService_GetAllConnections_GetSenderConnectionsWithoutPins_ReturnPaginationDto() {
+        Pageable pageable = PageRequest.of(0, 10);
+        Page<ConnectionDto> pageOfConnectionDto = new PageImpl<>(List.of(senderConnectionDto));
+
+        ChatMessage chatMessage = new ChatMessage();
+        chatMessage.setUser(sender);
+        connection.setChatMessages(List.of(chatMessage));
+
+        when(userService.getUserById(sender.getId())).thenReturn(sender);
+        when(userService.getCurrentlyLoggedInUser()).thenReturn(sender);
+        when(paginationService.getPageable(0, 10, "next")).thenReturn(pageable);
+        when(connectionPinService.getOwnerConnectionPins(sender.getId())).thenReturn(List.of(1L));
+        when(connectionRepository.getSenderConnectionsWithoutPins(pageable, sender.getId()))
+                .thenReturn(pageOfConnectionDto);
+        when(connectionRepository.findById(connection.getId())).thenReturn(Optional.of(connection));
+
+
+        PaginationDto<ConnectionDto> result =
+                connectionService.getAllConnections(sender.getId(), 0, 10, "next", "true");
+
+        Assertions.assertThat(result).isNotNull();
+        ConnectionDto resultDto = result.getItems().get(0);
+        Assertions.assertThat(resultDto.getFirstName())
+                .isEqualTo(senderConnectionDto.getFirstName());
+        Assertions.assertThat(resultDto.getLastName()).isEqualTo(senderConnectionDto.getLastName());
+        Assertions.assertThat(resultDto.getReceiverId())
+                .isEqualTo(senderConnectionDto.getReceiverId());
+        Assertions.assertThat(resultDto.getSenderId()).isEqualTo(senderConnectionDto.getSenderId());
+    }
+
+    @Test
+    public void ConnectionService_GetAllConnections_GetSenderConnections_ReturnPaginationDto() {
+        Pageable pageable = PageRequest.of(0, 10);
+        Page<ConnectionDto> pageOfConnectionDto = new PageImpl<>(List.of(senderConnectionDto));
+
+        ChatMessage chatMessage = new ChatMessage();
+        chatMessage.setUser(sender);
+        connection.setChatMessages(List.of(chatMessage));
+
+        when(userService.getUserById(sender.getId())).thenReturn(sender);
+        when(userService.getCurrentlyLoggedInUser()).thenReturn(sender);
+        when(paginationService.getPageable(0, 10, "next")).thenReturn(pageable);
+        when(connectionPinService.getOwnerConnectionPins(sender.getId())).thenReturn(List.of(1L));
+        when(connectionRepository.getSenderConnections(pageable, sender.getId(), List.of(1L)))
+                .thenReturn(pageOfConnectionDto);
+        when(connectionRepository.findById(connection.getId())).thenReturn(Optional.of(connection));
+
+
+        PaginationDto<ConnectionDto> result =
+                connectionService.getAllConnections(sender.getId(), 0, 10, "next", "false");
+
+        Assertions.assertThat(result).isNotNull();
+        ConnectionDto resultDto = result.getItems().get(0);
+        Assertions.assertThat(resultDto.getFirstName())
+                .isEqualTo(senderConnectionDto.getFirstName());
+        Assertions.assertThat(resultDto.getLastName()).isEqualTo(senderConnectionDto.getLastName());
+        Assertions.assertThat(resultDto.getReceiverId())
+                .isEqualTo(senderConnectionDto.getReceiverId());
+        Assertions.assertThat(resultDto.getSenderId()).isEqualTo(senderConnectionDto.getSenderId());
+    }
+
+    @Test
+    public void ConnectionService_GetAllConnections_GetReceiverConnections_ReturnPaginationDto() {
+        Pageable pageable = PageRequest.of(0, 10);
+        Page<ConnectionDto> pageOfConnectionDto = new PageImpl<>(List.of(receiverConnectionDto));
+
+        ChatMessage chatMessage = new ChatMessage();
+        chatMessage.setUser(sender);
+        connection.setChatMessages(List.of(chatMessage));
+
+        when(userService.getUserById(receiver.getId())).thenReturn(receiver);
+        when(userService.getCurrentlyLoggedInUser()).thenReturn(receiver);
+        when(paginationService.getPageable(0, 10, "next")).thenReturn(pageable);
+        when(connectionPinService.getOwnerConnectionPins(receiver.getId())).thenReturn(List.of(1L));
+        when(connectionRepository.getReceiverConnections(pageable, receiver.getId(), List.of(1L)))
+                .thenReturn(pageOfConnectionDto);
+        when(connectionRepository.findById(connection.getId())).thenReturn(Optional.of(connection));
+
+        PaginationDto<ConnectionDto> result =
+                connectionService.getAllConnections(receiver.getId(), 0, 10, "next", "false");
+
+        Assertions.assertThat(result).isNotNull();
+        ConnectionDto resultDto = result.getItems().get(0);
+        Assertions.assertThat(resultDto.getFirstName())
+                .isEqualTo(receiverConnectionDto.getFirstName());
+        Assertions.assertThat(resultDto.getLastName())
+                .isEqualTo(receiverConnectionDto.getLastName());
+        Assertions.assertThat(resultDto.getReceiverId())
+                .isEqualTo(receiverConnectionDto.getReceiverId());
+        Assertions.assertThat(resultDto.getSenderId())
+                .isEqualTo(receiverConnectionDto.getSenderId());
+    }
+
+    @Test
+    public void ConnectionService_GetAllConnections_GetReceiverConnectionsWithoutPins_ReturnPaginationDto() {
+        Pageable pageable = PageRequest.of(0, 10);
+        Page<ConnectionDto> pageOfConnectionDto = new PageImpl<>(List.of(receiverConnectionDto));
+
+        ChatMessage chatMessage = new ChatMessage();
+        chatMessage.setUser(sender);
+        connection.setChatMessages(List.of(chatMessage));
+
+        when(userService.getUserById(receiver.getId())).thenReturn(receiver);
+        when(userService.getCurrentlyLoggedInUser()).thenReturn(receiver);
+        when(paginationService.getPageable(0, 10, "next")).thenReturn(pageable);
+        when(connectionPinService.getOwnerConnectionPins(receiver.getId())).thenReturn(List.of());
+        when(connectionRepository.getReceiverConnectionsWithoutPins(pageable, receiver.getId()))
+                .thenReturn(pageOfConnectionDto);
+        when(connectionRepository.findById(connection.getId())).thenReturn(Optional.of(connection));
+
+
+        PaginationDto<ConnectionDto> result =
+                connectionService.getAllConnections(receiver.getId(), 0, 10, "next", "true");
+
+        Assertions.assertThat(result).isNotNull();
+        ConnectionDto resultDto = result.getItems().get(0);
+        Assertions.assertThat(resultDto.getFirstName())
+                .isEqualTo(receiverConnectionDto.getFirstName());
+        Assertions.assertThat(resultDto.getLastName())
+                .isEqualTo(receiverConnectionDto.getLastName());
+        Assertions.assertThat(resultDto.getReceiverId())
+                .isEqualTo(receiverConnectionDto.getReceiverId());
+        Assertions.assertThat(resultDto.getSenderId())
+                .isEqualTo(receiverConnectionDto.getSenderId());
+    }
+
+
+
+    @Test
+    public void ConnectionService_GetAllSearchConnections_GetReceiver_ReturnPaginationDto() {
+        Pageable pageable = PageRequest.of(0, 10);
+        Page<ConnectionDto> pageOfConnectionDto = new PageImpl<>(List.of(receiverConnectionDto));
+
+        ChatMessage chatMessage = new ChatMessage();
+        chatMessage.setUser(receiver);
+        connection.setChatMessages(List.of(chatMessage));
+
+        when(userService.getCurrentlyLoggedInUser()).thenReturn(receiver);
+        when(paginationService.getPageable(0, 10, "next")).thenReturn(pageable);
+        when(connectionRepository.getSearchReceiverConnections(pageable, receiver.getId(),
+                "%john%")).thenReturn(pageOfConnectionDto);
+        when(connectionRepository.findById(connection.getId())).thenReturn(Optional.of(connection));
+
+        PaginationDto<ConnectionDto> result =
+                connectionService.getAllSearchConnections("john", 0, 10, "next");
+
+        Assertions.assertThat(result).isNotNull();
+        ConnectionDto resultDto = result.getItems().get(0);
+        Assertions.assertThat(resultDto.getFirstName())
+                .isEqualTo(receiverConnectionDto.getFirstName());
+        Assertions.assertThat(resultDto.getLastName())
+                .isEqualTo(receiverConnectionDto.getLastName());
+        Assertions.assertThat(resultDto.getReceiverId())
+                .isEqualTo(receiverConnectionDto.getReceiverId());
+        Assertions.assertThat(resultDto.getSenderId())
+                .isEqualTo(receiverConnectionDto.getSenderId());
+    }
+
+    @Test
+    public void ConnectionService_GetAllSearchConnections_GetSender_ReturnPaginationDto() {
+        Pageable pageable = PageRequest.of(0, 10);
+        Page<ConnectionDto> pageOfConnectionDto = new PageImpl<>(List.of(senderConnectionDto));
+
+        ChatMessage chatMessage = new ChatMessage();
+        chatMessage.setUser(sender);
+        connection.setChatMessages(List.of(chatMessage));
+
+        when(userService.getCurrentlyLoggedInUser()).thenReturn(sender);
+        when(paginationService.getPageable(0, 10, "next")).thenReturn(pageable);
+        when(connectionRepository.getSearchSenderConnections(pageable, sender.getId(), "%jane%"))
+                .thenReturn(pageOfConnectionDto);
+        when(connectionRepository.findById(connection.getId())).thenReturn(Optional.of(connection));
+
+        PaginationDto<ConnectionDto> result =
+                connectionService.getAllSearchConnections("jane", 0, 10, "next");
+
+        Assertions.assertThat(result).isNotNull();
+        ConnectionDto resultDto = result.getItems().get(0);
+        Assertions.assertThat(resultDto.getFirstName())
+                .isEqualTo(senderConnectionDto.getFirstName());
+        Assertions.assertThat(resultDto.getLastName()).isEqualTo(senderConnectionDto.getLastName());
+        Assertions.assertThat(resultDto.getReceiverId())
+                .isEqualTo(senderConnectionDto.getReceiverId());
+        Assertions.assertThat(resultDto.getSenderId()).isEqualTo(senderConnectionDto.getSenderId());
+    }
 }
 
