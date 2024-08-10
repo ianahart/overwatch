cb43f0861f1d4eeb3acd4014d8b44003cf379b13
Ian Hart <ianhart7790@gmail.com>
2024-08-10 16:59:04 -0400

    test: add tests in the connection controller for getting all connections and searching connections

--- src/test/java/com/hart/overwatch/connection/ConnectionControllerTest.java
+++ src/test/java/com/hart/overwatch/connection/ConnectionControllerTest.java
@@ -6,8 +6,7 @@ import com.hart.overwatch.config.JwtService;
 import com.hart.overwatch.connection.dto.ConnectionDto;
 import com.hart.overwatch.connection.dto.MinConnectionDto;
 import com.hart.overwatch.connection.request.CreateConnectionRequest;
-import com.hart.overwatch.location.dto.LocationDto;
-import com.hart.overwatch.location.request.CreateLocationRequest;
+import com.hart.overwatch.pagination.dto.PaginationDto;
 import com.hart.overwatch.profile.Profile;
 import com.hart.overwatch.location.Location;
 import com.hart.overwatch.setting.Setting;
@@ -22,6 +21,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.test.context.ActiveProfiles;
@@ -32,6 +34,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import java.util.Collections;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import org.hamcrest.CoreMatchers;
@@ -67,8 +70,6 @@ public class ConnectionControllerTest {
     private User receiver;
 
     private ConnectionDto senderConnectionDto;
-
-    private ConnectionDto receiverConnectionDto;
 
     private User createSender() {
         Boolean loggedIn = false;
@@ -125,12 +126,6 @@ public class ConnectionControllerTest {
 
         connection = new Connection(RequestStatus.ACCEPTED, sender, receiver);
         connection.setId(1L);
-
-        receiverConnectionDto = new ConnectionDto(connection.getId(), receiver.getId(),
-                sender.getId(), sender.getFirstName(), sender.getLastName(),
-                sender.getProfile().getAvatarUrl(), sender.getProfile().getEmail(),
-                sender.getLocation().getCity(), sender.getLocation().getCountry(),
-                sender.getProfile().getContactNumber(), sender.getProfile().getBio());
 
         senderConnectionDto = new ConnectionDto(connection.getId(), receiver.getId(),
                 sender.getId(), receiver.getFirstName(), receiver.getLastName(),
@@ -189,22 +184,97 @@ public class ConnectionControllerTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("$.message", CoreMatchers.is("success")));
     }
 
-    // @Test
-    // public void LocationController_GetAllLocations_ReturnGetLocationAutoCompleteResponse ()
-    // throws Exception {
-    // when(locationService.getLocationAutoComplete("42 main")).thenReturn("42 main street");
-    //
-    // ResultActions response =
-    // mockMvc.perform(get("/api/v1/locations/autocomplete").contentType(MediaType.APPLICATION_JSON).param("text",
-    // "42 main"));
-    //
-    // response.andExpect(MockMvcResultMatchers.status().isOk())
-    // .andExpect(MockMvcResultMatchers.jsonPath("$.message", CoreMatchers.is("success")))
-    // .andExpect(MockMvcResultMatchers.jsonPath("$.data", CoreMatchers.is("42 main street")));
-    // }
-    //
+    @Test
+    public void ConnectionController_GetAllConnections_ReturnGetAllConnectionsResponse()
+            throws Exception {
+        int page = 0;
+        int pageSize = 3;
+        String direction = "next";
+        String override = "false";
+        Pageable pageable = Pageable.ofSize(pageSize);;
+        Page<ConnectionDto> pageResult =
+                new PageImpl<>(Collections.singletonList(senderConnectionDto), pageable, 1);
+        PaginationDto<ConnectionDto> paginationDto =
+                new PaginationDto<>(pageResult.getContent(), pageResult.getNumber(), pageSize,
+                        pageResult.getTotalPages(), direction, pageResult.getTotalElements());
 
 
+
+        when(connectionService.getAllConnections(sender.getId(), page, pageSize, direction,
+                override)).thenReturn(paginationDto);
+
+        ResultActions response =
+                mockMvc.perform(get("/api/v1/connections").contentType(MediaType.APPLICATION_JSON)
+                        .param("override", "false").param("userId", "1").param("page", "0")
+                        .param("pageSize", "3").param("direction", "next"));
+
+        response.andExpect(MockMvcResultMatchers.status().isOk());
+        response.andExpect(MockMvcResultMatchers.jsonPath("$.message", CoreMatchers.is("success")));
+        response.andExpect(
+                MockMvcResultMatchers.jsonPath("$.data.items[0].senderId", CoreMatchers.is(1)));
+        response.andExpect(MockMvcResultMatchers.jsonPath("$.data.items[0].receiverId",
+                CoreMatchers.is(senderConnectionDto.getReceiverId().intValue())));
+        response.andExpect(MockMvcResultMatchers.jsonPath("$.data.items[0].avatarUrl",
+                CoreMatchers.is(senderConnectionDto.getAvatarUrl())));
+        response.andExpect(MockMvcResultMatchers.jsonPath("$.data.items[0].firstName",
+                CoreMatchers.is(senderConnectionDto.getFirstName())));
+        response.andExpect(MockMvcResultMatchers.jsonPath("$.data.items[0].lastName",
+                CoreMatchers.is(senderConnectionDto.getLastName())));
+        response.andExpect(MockMvcResultMatchers.jsonPath("$.data.items[0].email",
+                CoreMatchers.is(senderConnectionDto.getEmail())));
+        response.andExpect(MockMvcResultMatchers.jsonPath("$.data.items[0].country",
+                CoreMatchers.is(senderConnectionDto.getCountry())));
+        response.andExpect(MockMvcResultMatchers.jsonPath("$.data.items[0].city",
+                CoreMatchers.is(senderConnectionDto.getCity())));
+        response.andExpect(MockMvcResultMatchers.jsonPath("$.data.items[0].bio",
+                CoreMatchers.is(senderConnectionDto.getBio())));
+
+    }
+
+    @Test
+    public void ConnectionController_GetAllSearchConnections_ReturnGetAllSearchConnectionsResponse()
+            throws Exception {
+        int page = 0;
+        int pageSize = 3;
+        String direction = "next";
+        String query = "jane";
+        Pageable pageable = Pageable.ofSize(pageSize);;
+        Page<ConnectionDto> pageResult =
+                new PageImpl<>(Collections.singletonList(senderConnectionDto), pageable, 1);
+        PaginationDto<ConnectionDto> paginationDto =
+                new PaginationDto<>(pageResult.getContent(), pageResult.getNumber(), pageSize,
+                        pageResult.getTotalPages(), direction, pageResult.getTotalElements());
+
+
+        when(connectionService.getAllSearchConnections(query, page, pageSize, direction))
+                .thenReturn(paginationDto);
+
+
+        ResultActions response = mockMvc.perform(get("/api/v1/connections/search")
+                .contentType(MediaType.APPLICATION_JSON).param("query", "jane").param("userId", "1")
+                .param("page", "0").param("pageSize", "3").param("direction", "next"));
+
+        response.andExpect(MockMvcResultMatchers.status().isOk());
+        response.andExpect(MockMvcResultMatchers.jsonPath("$.message", CoreMatchers.is("success")));
+        response.andExpect(
+                MockMvcResultMatchers.jsonPath("$.data.items[0].senderId", CoreMatchers.is(1)));
+        response.andExpect(MockMvcResultMatchers.jsonPath("$.data.items[0].receiverId",
+                CoreMatchers.is(senderConnectionDto.getReceiverId().intValue())));
+        response.andExpect(MockMvcResultMatchers.jsonPath("$.data.items[0].avatarUrl",
+                CoreMatchers.is(senderConnectionDto.getAvatarUrl())));
+        response.andExpect(MockMvcResultMatchers.jsonPath("$.data.items[0].firstName",
+                CoreMatchers.is(senderConnectionDto.getFirstName())));
+        response.andExpect(MockMvcResultMatchers.jsonPath("$.data.items[0].lastName",
+                CoreMatchers.is(senderConnectionDto.getLastName())));
+        response.andExpect(MockMvcResultMatchers.jsonPath("$.data.items[0].email",
+                CoreMatchers.is(senderConnectionDto.getEmail())));
+        response.andExpect(MockMvcResultMatchers.jsonPath("$.data.items[0].country",
+                CoreMatchers.is(senderConnectionDto.getCountry())));
+        response.andExpect(MockMvcResultMatchers.jsonPath("$.data.items[0].city",
+                CoreMatchers.is(senderConnectionDto.getCity())));
+        response.andExpect(MockMvcResultMatchers.jsonPath("$.data.items[0].bio",
+                CoreMatchers.is(senderConnectionDto.getBio())));
+    }
 
 }
 
