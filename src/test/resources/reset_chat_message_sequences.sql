40d422ad196aeedf87a3b9bece7183ad84810547
Ian Hart <ianhart7790@gmail.com>
2024-08-11 17:02:07 -0400

    test: setup data for chat message repository tests

---
+++ src/test/resources/reset_chat_message_sequences.sql
@@ -0,0 +1,6 @@
+ALTER SEQUENCE chat_message_sequence RESTART WITH 1;
+ALTER SEQUENCE connection_sequence RESTART WITH 1;
+ALTER SEQUENCE location_sequence RESTART WITH 1;
+ALTER SEQUENCE profile_sequence RESTART WITH 1;
+ALTER SEQUENCE _user_sequence RESTART WITH 1;
+
