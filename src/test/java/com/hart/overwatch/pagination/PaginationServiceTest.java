0b226132729435279ae7376532a910adc15808c4
Ian Hart <ianhart7790@gmail.com>
2024-08-14 12:10:42 -0400

    test: add pagination service test for paginating a page

---
+++ src/test/java/com/hart/overwatch/pagination/PaginationServiceTest.java
@@ -0,0 +1,52 @@
+package com.hart.overwatch.pagination;
+
+import org.mockito.ArgumentCaptor;
+import static org.mockito.ArgumentCaptor.forClass;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@ExtendWith(MockitoExtension.class)
+public class PaginationServiceTest {
+
+    @InjectMocks
+    private PaginationService paginationService;
+
+    @BeforeEach
+    void setUp() {}
+
+
+    @Test
+    public void PaginationService_PaginateNext_ReturnInt() {
+        int page = 0;
+        String direction = "next";
+
+        int result = paginationService.paginate(page, direction);
+
+        Assertions.assertThat(result).isEqualTo(1);
+    }
+
+    @Test
+    public void PaginationService_PaginatePrev_ReturnInt() {
+        int page = 2;
+        String direction = "prev";
+
+        int result = paginationService.paginate(page, direction);
+
+        Assertions.assertThat(result).isEqualTo(1);
+    }
+}
+
+
