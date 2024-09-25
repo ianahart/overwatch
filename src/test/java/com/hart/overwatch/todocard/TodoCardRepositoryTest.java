747b84dce82e0593e5f0ab29747925ba564395cf
Ian Hart <ianhart7790@gmail.com>
2024-09-25 12:04:39 -0400

    test: add repository for retrieving all a user's todocards by user id and todo list id

---
+++ src/test/java/com/hart/overwatch/todocard/TodoCardRepositoryTest.java
@@ -0,0 +1,109 @@
+package com.hart.overwatch.todocard;
+
+import org.assertj.core.api.Assertions;
+import org.hamcrest.core.IsEqual;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.transaction.annotation.Transactional;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import com.hart.overwatch.profile.Profile;
+import com.hart.overwatch.setting.Setting;
+import com.hart.overwatch.todocard.dto.TodoCardDto;
+import com.hart.overwatch.todolist.TodoList;
+import com.hart.overwatch.todolist.TodoListRepository;
+import com.hart.overwatch.todolist.dto.TodoListDto;
+import com.hart.overwatch.user.Role;
+import com.hart.overwatch.user.User;
+import com.hart.overwatch.user.UserRepository;
+import com.hart.overwatch.workspace.WorkSpace;
+import java.util.List;
+
+@DataJpaTest
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Sql(scripts = "classpath:reset_todo_list_sequences.sql",
+        executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Transactional
+public class TodoCardRepositoryTest {
+
+    @Autowired
+    private TodoCardRepository todoCardRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    TodoListRepository todoListRepository;
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    private User user;
+
+    private WorkSpace workSpace;
+
+    private TodoList todoList;
+
+    @BeforeEach
+    public void setUp() {
+        Boolean loggedIn = true;
+        user = new User("john@mail.com", "John", "Doe", "John Doe", Role.USER, loggedIn,
+                new Profile(), "Test12345%", new Setting());
+
+        userRepository.save(user);
+
+        todoList = new TodoList(user, workSpace, "todo list title", 0);
+        todoListRepository.save(todoList);
+
+        createTodoCards(user, todoList);
+
+    }
+
+
+    private void createTodoCards(User user, TodoList todoList) {
+        int numOfCards = 3;
+        for (int i = 1; i <= numOfCards; i++) {
+            TodoCard todoCard = new TodoCard();
+            todoCard.setId(Long.valueOf(i));
+            todoCard.setUser(user);
+            todoCard.setTodoList(todoList);
+            todoCard.setTitle(String.format("Card-%d", i));
+            todoCardRepository.save(todoCard);
+        }
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.out.println("Tearing down the test data...");
+        todoCardRepository.deleteAll();
+        todoListRepository.deleteAll();
+        userRepository.deleteAll();
+        entityManager.flush();
+        entityManager.clear();
+    }
+
+    @Test
+    public void TodoCardRepository_RetrieveTodoCards_ReturnTodoCardDto() {
+        List<TodoCardDto> todoCardDtos =
+                todoCardRepository.retrieveTodoCards(todoList.getId(), user.getId());
+
+        Assertions.assertThat(todoCardDtos).isNotNull();
+        Assertions.assertThat(todoCardDtos.size()).isEqualTo(3);
+        Assertions.assertThat(todoCardDtos.get(0).getId()).isEqualTo(1L);
+        Assertions.assertThat(todoCardDtos.get(1).getId()).isEqualTo(2L);
+        Assertions.assertThat(todoCardDtos.get(2).getId()).isEqualTo(3L);
+    }
+}
+
+
