2edc4e6d7056fc552c2b835ae33ab4e644c84be6
Ian Hart <ianhart7790@gmail.com>
2024-09-27 12:12:09 -0400

    test: add sql script for resetting active label sequences after each repository test for active labels

---
+++ src/test/resources/reset_active_label_sequences.sql
@@ -0,0 +1,5 @@
+ALTER SEQUENCE active_label_sequence RESTART WITH 1;
+ALTER SEQUENCE workspace_sequence RESTART WITH 1;
+ALTER SEQUENCE todo_card_sequence RESTART WITH 1;
+ALTER SEQUENCE _user_sequence RESTART WITH 1;
+
