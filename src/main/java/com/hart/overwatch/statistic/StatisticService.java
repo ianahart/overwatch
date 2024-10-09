d85b23b3b6ba002ea8293031b563b26ace6159ae
Ian Hart <ianhart7790@gmail.com>
2024-10-09 11:51:31 -0400

    feat: add avg review time data

--- src/main/java/com/hart/overwatch/statistic/StatisticService.java
+++ src/main/java/com/hart/overwatch/statistic/StatisticService.java
@@ -1,8 +1,12 @@
 package com.hart.overwatch.statistic;
 
+import java.util.Map;
+import java.util.HashMap;
+import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.YearMonth;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -37,12 +41,13 @@ public class StatisticService {
     private List<ReviewTypeStatDto> getReviewTypesCompleted(Long reviewerId) {
         List<CompletedRepositoryReviewDto> reviews =
                 repositoryService.getCompletedReviews(reviewerId);
-           return reviews.stream()
-            .collect(Collectors.groupingBy(CompletedRepositoryReviewDto::getReviewType))
-            .entrySet().stream()
-            .<ReviewTypeStatDto>map(entry -> new ReviewTypeStatDto(entry.getKey(), entry.getValue().size()))
-            .collect(Collectors.toList());
-}
+        return reviews.stream()
+                .collect(Collectors.groupingBy(CompletedRepositoryReviewDto::getReviewType))
+                .entrySet().stream()
+                .<ReviewTypeStatDto>map(
+                        entry -> new ReviewTypeStatDto(entry.getKey(), entry.getValue().size()))
+                .collect(Collectors.toList());
+    }
 
     private List<CompletedReviewStatDto> getReviewsCompleted(Long reviewerId) {
         LocalDateTime startOfMonth = LocalDate.now().withDayOfMonth(1).atStartOfDay();
@@ -62,12 +67,34 @@ public class StatisticService {
 
     }
 
+    private List<Map<String, Object>> getAverageReviewTimes(Long reviewerId) {
+        List<CompletedRepositoryReviewDto> reviews =
+                repositoryService.getCompletedReviews(reviewerId);
+        Map<YearMonth, List<CompletedRepositoryReviewDto>> reviewsByMonth =
+                reviews.stream().collect(
+                        Collectors.groupingBy(review -> YearMonth.from(review.getReviewEndTime())));
+        Map<YearMonth, Double> averageReviewTimeByMonth = reviewsByMonth.entrySet().stream()
+                .collect(Collectors.toMap(Map.Entry::getKey,
+                        entry -> entry.getValue().stream()
+                                .mapToLong(review -> Duration.between(review.getReviewStartTime(),
+                                        review.getReviewEndTime()).toMinutes())
+                                .average().orElse(0)));
+
+        return averageReviewTimeByMonth.entrySet().stream().map(entry -> {
+            Map<String, Object> data = new HashMap<>();
+            data.put("month", entry.getKey().toString());
+            data.put("avgReviewTime", entry.getValue());
+            return data;
+        }).collect(Collectors.toList());
+    }
+
     public OverallStatDto getStatistics(Long reviewerId) {
         if (reviewerId == null) {
             throw new BadRequestException("Missing parameter: reviewerId");
         }
         List<CompletedReviewStatDto> reviewsCompleted = getReviewsCompleted(reviewerId);
         List<ReviewTypeStatDto> reviewTypesCompleted = getReviewTypesCompleted(reviewerId);
-        return new OverallStatDto(reviewsCompleted, reviewTypesCompleted);
+        List<Map<String, Object>> avgReviewTimes = getAverageReviewTimes(reviewerId);
+        return new OverallStatDto(reviewsCompleted, reviewTypesCompleted, avgReviewTimes);
     }
 }
