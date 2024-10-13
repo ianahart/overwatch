1a7c941a9dc0b88eabf48f8fdcb3336a235c3ffe
Ian Hart <ianhart7790@gmail.com>
2024-10-13 14:01:25 -0400

    test: add a repository test for review feedback for getting a lsit of review feedback ratings

---
+++ src/test/resources/reset_review_feedback_sequences.sql
@@ -0,0 +1,5 @@
+ALTER SEQUENCE review_feedback_sequence RESTART WITH 1;
+ALTER SEQUENCE repository_sequence RESTART WITH 1;
+ALTER SEQUENCE profile_sequence RESTART WITH 1;
+ALTER SEQUENCE _user_sequence RESTART WITH 1;
+
