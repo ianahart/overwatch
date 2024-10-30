8e73cf1d94d4fd160479559949ed535fa3e7d7f9
Ian Hart <ianhart7790@gmail.com>
2024-10-30 16:57:29 -0400

    feat: able to share current url copiedto clipboard with share button on topic details page

--- frontend/src/components/TopicDetails/TopicDetailsStats.tsx
+++ frontend/src/components/TopicDetails/TopicDetailsStats.tsx
@@ -1,11 +1,36 @@
 import { BiComment } from 'react-icons/bi';
 import { FaShare } from 'react-icons/fa';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export interface ITopicDetailsStatsProps {
   totalCommentCount: number;
 }
 
 const TopicDetailsStats = ({ totalCommentCount }: ITopicDetailsStatsProps) => {
+  const handleCopyUrl = async () => {
+    try {
+      const currentUrl = window.location.href;
+      await navigator.clipboard.writeText(currentUrl);
+      initiateToast();
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const initiateToast = () => {
+    toast.success('The current url has been copied to your clipboard!', {
+      position: 'bottom-center',
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: 'dark',
+    });
+  };
+
   return (
     <div className="my-12 w-full">
       <div className="flex justify-start">
@@ -17,7 +42,10 @@ const TopicDetailsStats = ({ totalCommentCount }: ITopicDetailsStatsProps) => {
             <p>{totalCommentCount}</p>
           </div>
         </div>
-        <div className="m-2 flex items-center bg-gray-800 p-2 rounded-2xl justify-between cursor-pointer">
+        <div
+          onClick={handleCopyUrl}
+          className="m-2 flex items-center bg-gray-800 p-2 rounded-2xl justify-between cursor-pointer"
+        >
           <div className="mx-1">
             <FaShare />
           </div>
@@ -26,6 +54,7 @@ const TopicDetailsStats = ({ totalCommentCount }: ITopicDetailsStatsProps) => {
           </div>
         </div>
       </div>
+      <ToastContainer />
     </div>
   );
 };
