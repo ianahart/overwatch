2cda7cdc80903ae491331114e0c6af29f6047369
Ian Hart <ianhart7790@gmail.com>
2024-11-01 13:00:38 -0400

    test: add a controller test for getting all  comments

--- src/test/java/com/hart/overwatch/comment/CommentControllerTest.java
+++ src/test/java/com/hart/overwatch/comment/CommentControllerTest.java
@@ -1,23 +1,20 @@
 package com.hart.overwatch.comment;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collections;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hart.overwatch.comment.dto.CommentDto;
-import com.hart.overwatch.config.DatabaseSetupService;
 import com.hart.overwatch.config.JwtService;
 import com.hart.overwatch.pagination.dto.PaginationDto;
 import com.hart.overwatch.profile.Profile;
+import com.hart.overwatch.reaction.dto.ReactionDto;
 import com.hart.overwatch.setting.Setting;
 import com.hart.overwatch.tag.Tag;
-import com.hart.overwatch.tag.dto.TagDto;
 import com.hart.overwatch.token.TokenRepository;
 import com.hart.overwatch.topic.Topic;
-import com.hart.overwatch.topic.dto.TopicDto;
-import com.hart.overwatch.topic.request.CreateTopicRequest;
-import com.hart.overwatch.topicmanagement.TopicManagementService;
 import com.hart.overwatch.user.Role;
 import com.hart.overwatch.user.User;
 import org.junit.jupiter.api.BeforeEach;
@@ -42,6 +39,8 @@ import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import org.hamcrest.CoreMatchers;
 import org.hamcrest.Matchers;
 
@@ -153,6 +152,48 @@ public class CommentControllerTest {
         tags.forEach(tag -> tag.setTopics(List.of(topic)));
     }
 
+    @Test
+    public void CommentController_GetAllCommentResponse_ReturnGetAllCommentResponse()
+            throws Exception {
+        int page = 0;
+        int pageSize = 3;
+        String direction = "next";
+        String sort = "vote";
+
+        Pageable pageable = Pageable.ofSize(pageSize).withPage(page);
+        List<CommentDto> commentDtos =
+                comments.stream().map(this::convertToDto).collect(Collectors.toList());
+        Page<CommentDto> pageResult = new PageImpl<>(commentDtos, pageable, commentDtos.size());
+
+        PaginationDto<CommentDto> expectedPaginationDto =
+                new PaginationDto<>(commentDtos, pageResult.getNumber(), pageSize,
+                        pageResult.getTotalPages(), direction, pageResult.getTotalElements());
+
+        when(commentService.getComments(topic.getId(), page, pageSize, direction, sort))
+                .thenReturn(expectedPaginationDto);
+
+        ResultActions response =
+                mockMvc.perform(get(String.format("/api/v1/topics/%d/comments", topic.getId()))
+                        .param("page", "0").param("pageSize", "3").param("direction", direction)
+                        .param("sort", "vote"));
+
+
+        response.andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.message", CoreMatchers.is("success")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.items[0].id",
+                        CoreMatchers.is(commentDtos.get(0).getId().intValue())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.page",
+                        CoreMatchers.is(expectedPaginationDto.getPage())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.items",
+                        Matchers.hasSize(commentDtos.size())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.pageSize",
+                        CoreMatchers.is(expectedPaginationDto.getPageSize())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.totalElements",
+                        CoreMatchers.is((int) expectedPaginationDto.getTotalElements())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.direction",
+                        CoreMatchers.is(expectedPaginationDto.getDirection())));
+
+    }
 
 
 }
