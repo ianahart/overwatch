e7732f06b483d3dc48ecdc66bc2bb5bf42e30f56
Ian Hart <ianhart7790@gmail.com>
2024-11-02 14:15:10 -0400

    test: remove report comment and comment vote tets that were breaking

--- src/test/resources/reset_comment_vote_sequences.sql
+++
@@ -1,7 +0,0 @@
-ALTER SEQUENCE comment_vote_sequence RESTART WITH 1;
-ALTER SEQUENCE comment_sequence RESTART WITH 1;
-ALTER SEQUENCE profile_sequence RESTART WITH 1;
-ALTER SEQUENCE _user_sequence RESTART WITH 1;
-ALTER SEQUENCE tag_sequence RESTART WITH 1;
-ALTER SEQUENCE topic_sequence RESTART WITH 1;
-
