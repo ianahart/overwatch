49e247e1cf3f1127d5832fad524cc783b658e7fe
Ian Hart <ianhart7790@gmail.com>
2024-11-03 11:46:59 -0500

    chore: remove unused imports

--- src/test/java/com/hart/overwatch/reportcomment/ReportCommentServiceTest.java
+++ src/test/java/com/hart/overwatch/reportcomment/ReportCommentServiceTest.java
@@ -16,7 +16,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import com.hart.overwatch.advice.BadRequestException;
 import com.hart.overwatch.comment.Comment;
 import com.hart.overwatch.comment.CommentService;
-import com.hart.overwatch.commentvote.request.CreateCommentVoteRequest;
 import com.hart.overwatch.profile.Profile;
 import com.hart.overwatch.reportcomment.request.CreateReportCommentRequest;
 import com.hart.overwatch.setting.Setting;
