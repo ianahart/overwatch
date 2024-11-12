a8b2be9d1f57652949be0b20e7ea6b8b2605a5be
Ian Hart <ianhart7790@gmail.com>
2024-11-12 16:57:52 -0500

    feat: send emails notifying the user and the reviewer that a transaction has been completed

--- src/main/java/com/hart/overwatch/stripepaymentintent/StripePaymentIntentService.java
+++ src/main/java/com/hart/overwatch/stripepaymentintent/StripePaymentIntentService.java
@@ -1,6 +1,8 @@
 package com.hart.overwatch.stripepaymentintent;
 
 import org.springframework.stereotype.Service;
+import com.hart.overwatch.email.EmailQueueService;
+import com.hart.overwatch.email.request.EmailRequest;
 import com.hart.overwatch.user.User;
 import com.hart.overwatch.user.UserService;
 import com.stripe.model.PaymentIntent;
@@ -12,11 +14,15 @@ public class StripePaymentIntentService {
 
     private final StripePaymentIntentRepository stripePaymentIntentRepository;
 
+    private final EmailQueueService emailQueueService;
+
 
     public StripePaymentIntentService(UserService userService,
-            StripePaymentIntentRepository stripePaymentIntentRepository) {
+            StripePaymentIntentRepository stripePaymentIntentRepository,
+            EmailQueueService emailQueueService) {
         this.userService = userService;
         this.stripePaymentIntentRepository = stripePaymentIntentRepository;
+        this.emailQueueService = emailQueueService;
     }
 
 
@@ -34,5 +40,27 @@ public class StripePaymentIntentService {
         stripePaymentIntent.setDescription(paymentIntent.getDescription());
 
         stripePaymentIntentRepository.save(stripePaymentIntent);
+        String reviewerText =
+                String.format("%s has paid you $%s %s for reviewing their code!\n package:%s",
+                        user.getFullName(), convertCentsToDollars(paymentIntent.getAmount()),
+                        paymentIntent.getCurrency(), paymentIntent.getDescription());
+        String userText = String.format("You paid %s $%s %s for reviewing your code!\n package: %s",
+                reviewer.getFullName(), convertCentsToDollars(paymentIntent.getAmount()),
+                paymentIntent.getCurrency(), paymentIntent.getDescription());
+        queueEmail(reviewer, "Payment Recieved", reviewerText);
+        queueEmail(user, "Payment Notification", userText);
+    }
+
+    private String convertCentsToDollars(Long cents) {
+        double dollars = cents / 100.0;
+
+        return String.format("$%.2f", dollars);
+    }
+
+    private void queueEmail(User recipient, String subject, String body) {
+        if (body != null && !body.isEmpty()) {
+            EmailRequest emailRequest = new EmailRequest(recipient.getEmail(), subject, body);
+            emailQueueService.queueEmail(emailRequest);
+        }
     }
 }
