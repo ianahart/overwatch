831cb605fbf5edc3673c5327416427fadac66190
Ian Hart <ianhart7790@gmail.com>
2024-12-02 17:49:56 -0500

    test: setup test data for team member repository

---
+++ src/test/java/com/hart/overwatch/teammember/TeamMemberRepositoryTest.java
@@ -0,0 +1,113 @@
+package com.hart.overwatch.teammember;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.transaction.annotation.Transactional;
+
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import com.hart.overwatch.config.DatabaseSetupService;
+import com.hart.overwatch.profile.Profile;
+import com.hart.overwatch.profile.ProfileRepository;
+import com.hart.overwatch.setting.Setting;
+import com.hart.overwatch.team.Team;
+import com.hart.overwatch.team.TeamRepository;
+import com.hart.overwatch.user.Role;
+import com.hart.overwatch.user.User;
+import com.hart.overwatch.user.UserRepository;
+
+@DataJpaTest
+@ActiveProfiles("test")
+@Import(DatabaseSetupService.class)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Sql(scripts = "classpath:reset_team_member_sequences.sql",
+        executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Transactional
+public class TeamMemberRepositoryTest {
+
+    @Autowired
+    private TeamMemberRepository teamMemberRepository;
+
+    @Autowired
+    private TeamRepository teamRepository;
+
+    @Autowired
+    private ProfileRepository profileRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    private User user;
+
+    private Team team;
+
+    private TeamMember teamMember;
+
+    private User createUser() {
+        Boolean loggedIn = true;
+        Profile profileEntity = new Profile();
+        profileRepository.save(profileEntity);
+        User userEntity = new User("john@mail.com", "John", "Doe", "John Doe", Role.USER, loggedIn,
+                profileEntity, "Test12345%", new Setting());
+        userRepository.save(userEntity);
+        return userEntity;
+    }
+
+    private Team createTeam(User user) {
+        Team teamEntity = new Team();
+        teamEntity.setUser(user);
+        teamEntity.setTeamName("team one");
+        teamRepository.save(teamEntity);
+
+        return teamEntity;
+    }
+
+    private TeamMember createTeamMember(User user, Team team) {
+        TeamMember teamMemberEntity = new TeamMember();
+        teamMemberEntity.setUser(user);
+        teamMemberEntity.setTeam(team);
+        teamMemberRepository.save(teamMemberEntity);
+
+        return teamMemberEntity;
+
+
+    }
+
+
+    @BeforeEach
+    public void setUp() {
+        user = createUser();
+        team = createTeam(user);
+        teamMember = createTeamMember(user, team);
+
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.out.println("Tearing down the test data...");
+        teamMemberRepository.deleteAll();
+        teamRepository.deleteAll();
+        profileRepository.deleteAll();
+        userRepository.deleteAll();
+        entityManager.flush();
+        entityManager.clear();
+    }
+
+}
+
+
