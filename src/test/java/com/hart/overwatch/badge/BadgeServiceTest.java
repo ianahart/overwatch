71dc17fd6f6d216c860a14e92206dc21b01cb75e
Ian Hart <ianhart7790@gmail.com>
2024-12-21 11:36:25 -0500

    test: add a badge service test for updating a badge with an image

--- src/test/java/com/hart/overwatch/badge/BadgeServiceTest.java
+++ src/test/java/com/hart/overwatch/badge/BadgeServiceTest.java
@@ -210,6 +210,36 @@ public class BadgeServiceTest {
         }).isInstanceOf(BadRequestException.class).hasMessage(String
                 .format("You have already created a badge with the title %s", request.getTitle()));
     }
+
+    @Test
+    public void BadgeService_UpdateBadge_WithImage_ReturnNothing() {
+        UpdateBadgeRequest request = new UpdateBadgeRequest();
+        request.setTitle("Second Reviewer Badge");
+        request.setDescription("description");
+        request.setImage(new MockMultipartFile("file", "test-image.jpeg", "image/jpeg",
+                new byte[] {(byte) 0xFF, (byte) 0xD8, (byte) 0xFF, (byte) 0xE0, 0x00, 0x10, 0x4A,
+                        0x46, 0x49, 0x46, 0x00, 0x01}));
+
+        when(badgeRepository.existsByTitle(request.getTitle().toLowerCase())).thenReturn(false);
+        when(badgeRepository.findById(badge.getId())).thenReturn(Optional.of(badge));
+
+        doNothing().when(amazonService).deleteBucketObject(BUCKET_NAME, "photo-1");
+        HashMap<String, String> result = new HashMap<>();
+        result.put("filename", "filename");
+        result.put("objectUrl", "https://www.s3.com/photo-1.jpeg");
+        when(amazonService.putS3Object(BUCKET_NAME, request.getImage().getOriginalFilename(),
+                request.getImage())).thenReturn(result);
+
+        when(badgeRepository.save(any(Badge.class))).thenReturn(badge);
+
+        badgeService.updateBadge(request, badge.getId());
+
+        verify(amazonService, times(1)).deleteBucketObject(BUCKET_NAME, "photo-1");
+        verify(amazonService, times(1)).putS3Object(BUCKET_NAME,
+                request.getImage().getOriginalFilename(), request.getImage());
+    }
+
+
 }
 
 
