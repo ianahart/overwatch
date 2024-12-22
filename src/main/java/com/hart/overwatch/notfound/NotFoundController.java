28b1ac69f2ad66bd66b23e7a0a3728b5ef49d77a
Ian Hart <ianhart7790@gmail.com>
2024-12-22 14:09:33 -0500

    fix: fix notfoundcontroller

--- src/main/java/com/hart/overwatch/notfound/NotFoundController.java
+++ src/main/java/com/hart/overwatch/notfound/NotFoundController.java
@@ -1,21 +1,32 @@
 package com.hart.overwatch.notfound;
 
+import org.springframework.web.HttpRequestHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.HandlerExecutionChain;
 import org.springframework.web.servlet.HandlerMapping;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 
 @RestController
 public class NotFoundController implements HandlerMapping {
     @Override
-    public HandlerExecutionChain getHandler(HttpServletRequest request) throws Exception {
+    public HandlerExecutionChain getHandler(HttpServletRequest request) {
         if ("/api/v1/".equals(request.getRequestURI())) {
-            return new HandlerExecutionChain((HttpServletRequest req, HttpServletResponse res) -> {
-                res.setStatus(HttpServletResponse.SC_NOT_FOUND);
-                res.getWriter().write("Not Found");
-                res.getWriter().flush();
-            });
+            HttpRequestHandler handler = new HttpRequestHandler() {
+                @Override
+                public void handleRequest(HttpServletRequest req, HttpServletResponse res) {
+                    try {
+                        res.setStatus(HttpServletResponse.SC_NOT_FOUND);
+                        res.getWriter().write("Not Found");
+                        res.getWriter().flush();
+                    } catch (IOException e) {
+                        throw new RuntimeException("Error handling request", e);
+                    }
+                }
+            };
+            return new HandlerExecutionChain(handler);
         }
         return null;
     }
