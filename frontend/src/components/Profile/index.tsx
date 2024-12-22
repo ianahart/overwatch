393ccf436bb7db27ad8c79231c94a756cbda1b9d
Ian Hart <ianhart7790@gmail.com>
2024-12-22 14:42:57 -0500

    test: commenting out code to see if i can pin point the api/v1/ error

--- frontend/src/components/Profile/index.tsx
+++ frontend/src/components/Profile/index.tsx
@@ -1,27 +1,28 @@
-//import { useFetchProfileQuery } from '../../state/store';
-//import UserProfile from './UserProfile';
+import { useFetchProfileQuery } from '../../state/store';
+// import UserProfile from './UserProfile';
 //import ReviewerProfile from './ReviewerProfile';
-//import { useEffect, useState } from 'react';
-//import { profileState } from '../../data';
-//import { IFullProfile } from '../../interfaces';
-//import { retrieveTokens } from '../../util';
-//
+import { useEffect, useState } from 'react';
+import { profileState } from '../../data';
+import { IFullProfile } from '../../interfaces';
+import { retrieveTokens } from '../../util';
+
 export interface IProfileProps {
   profileId: number;
 }
 
 const Profile = ({ profileId }: IProfileProps) => {
-  console.log(profileId);
-  //  const tokens = retrieveTokens();
-  //  const { data } = useFetchProfileQuery({ profileId, token: tokens.accessToken });
-  //  const [profile, setProfile] = useState<IFullProfile>(profileState);
-  //
-  //  useEffect(() => {
-  //    if (data) {
-  //      setProfile(data.data);
-  //    }
-  //  }, [data]);
-  //
+  const tokens = retrieveTokens();
+    console.log(tokens, profileId)
+  const { data } = useFetchProfileQuery({ profileId, token: tokens.accessToken });
+  const [profile, setProfile] = useState<IFullProfile>(profileState);
+    console.log(profile)
+
+  useEffect(() => {
+    if (data) {
+      setProfile(data.data);
+    }
+  }, [data]);
+
   return (
     <div>
       {/*     {profile.userProfile.role === 'REVIEWER' ? (
