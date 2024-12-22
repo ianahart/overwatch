6457fccca18ad26cd5cc7711d77fdf255c26df0e
Ian Hart <ianhart7790@gmail.com>
2024-12-22 14:37:53 -0500

    test: testing to see if request still goes through when handler is commented out

--- frontend/src/components/Profile/index.tsx
+++ frontend/src/components/Profile/index.tsx
@@ -1,33 +1,34 @@
-import { useFetchProfileQuery } from '../../state/store';
-import UserProfile from './UserProfile';
-import ReviewerProfile from './ReviewerProfile';
-import { useEffect, useState } from 'react';
-import { profileState } from '../../data';
-import { IFullProfile } from '../../interfaces';
-import { retrieveTokens } from '../../util';
-
+//import { useFetchProfileQuery } from '../../state/store';
+//import UserProfile from './UserProfile';
+//import ReviewerProfile from './ReviewerProfile';
+//import { useEffect, useState } from 'react';
+//import { profileState } from '../../data';
+//import { IFullProfile } from '../../interfaces';
+//import { retrieveTokens } from '../../util';
+//
 export interface IProfileProps {
   profileId: number;
 }
 
 const Profile = ({ profileId }: IProfileProps) => {
-  const tokens = retrieveTokens();
-  const { data } = useFetchProfileQuery({ profileId, token: tokens.accessToken });
-  const [profile, setProfile] = useState<IFullProfile>(profileState);
-
-  useEffect(() => {
-    if (data) {
-      setProfile(data.data);
-    }
-  }, [data]);
-
+  console.log(profileId);
+  //  const tokens = retrieveTokens();
+  //  const { data } = useFetchProfileQuery({ profileId, token: tokens.accessToken });
+  //  const [profile, setProfile] = useState<IFullProfile>(profileState);
+  //
+  //  useEffect(() => {
+  //    if (data) {
+  //      setProfile(data.data);
+  //    }
+  //  }, [data]);
+  //
   return (
     <div>
-      {profile.userProfile.role === 'REVIEWER' ? (
+      {/*     {profile.userProfile.role === 'REVIEWER' ? (
         <ReviewerProfile profile={profile} />
       ) : (
         <UserProfile profile={profile} />
-      )}
+      )}*/}
     </div>
   );
 };
