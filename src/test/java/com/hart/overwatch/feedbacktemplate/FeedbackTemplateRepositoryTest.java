b18fdfe0bffa7f8235b259b39508df66fe2e503d
Ian Hart <ianhart7790@gmail.com>
2025-01-26 12:03:16 -0500

    test: add a feedback template test for counting feedback templates by user id

---
+++ src/test/java/com/hart/overwatch/feedbacktemplate/FeedbackTemplateRepositoryTest.java
@@ -0,0 +1,104 @@
+package com.hart.overwatch.feedbacktemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.transaction.annotation.Transactional;
+
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import com.hart.overwatch.config.DatabaseSetupService;
+import com.hart.overwatch.profile.Profile;
+import com.hart.overwatch.setting.Setting;
+import com.hart.overwatch.topic.Topic;
+import com.hart.overwatch.topic.TopicRepository;
+import com.hart.overwatch.user.Role;
+import com.hart.overwatch.user.User;
+import com.hart.overwatch.user.UserRepository;
+
+@DataJpaTest
+@ActiveProfiles("test")
+@Import(DatabaseSetupService.class)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Sql(scripts = "classpath:reset_feedback_template_sequences.sql",
+        executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Transactional
+public class FeedbackTemplateRepositoryTest {
+
+    @Autowired
+    private FeedbackTemplateRepository feedbackTemplateRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    private User user;
+
+    private List<FeedbackTemplate> feedbackTemplates = new ArrayList<>();
+
+
+    private User createUser() {
+        Boolean loggedIn = true;
+        User userEntity = new User("john@mail.com", "John", "Doe", "John Doe", Role.USER, loggedIn,
+                new Profile(), "Test12345%", new Setting());
+        userRepository.save(userEntity);
+        return userEntity;
+    }
+
+
+    private List<FeedbackTemplate> createFeedbackTemplates(User user) {
+        List<FeedbackTemplate> feedbackTemplateEntities = new ArrayList<>();
+        for (int i = 0; i < 2; i++) {
+            FeedbackTemplate feedbackTemplateEntity = new FeedbackTemplate();
+            feedbackTemplateEntity.setUser(user);
+            feedbackTemplateEntity.setFeedback("feedback text");
+            feedbackTemplateEntities.add(feedbackTemplateEntity);
+        }
+
+
+        feedbackTemplateRepository.saveAll(feedbackTemplateEntities);
+        return feedbackTemplateEntities;
+    }
+
+    @BeforeEach
+    public void setUp() {
+        user = createUser();
+        feedbackTemplates = createFeedbackTemplates(user);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.out.println("Tearing down the test data...");
+        feedbackTemplateRepository.deleteAll();
+        userRepository.deleteAll();
+        entityManager.flush();
+        entityManager.clear();
+    }
+
+
+    @Test
+    public void FeedbackTemplateRepository_CountFeedbackTemplatesByUserId_ReturnIntCount() {
+        Long userId = user.getId();
+
+        int count = feedbackTemplateRepository.countFeedbackTemplatesByUserId(userId);
+
+        Assertions.assertThat(count).isEqualTo(2);
+
+    }
+
+}
+
+
