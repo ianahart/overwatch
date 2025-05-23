f42e1a99d3bfba1736afcac4387a877229d4549e
Ian Hart <ianhart7790@gmail.com>
2025-05-23 19:22:03 -0400

    test: add a test suite for the TopicDetailsCommentItemForm component inside the TopicDetails package

--- frontend/tests/handlers/comments.ts
+++ frontend/tests/handlers/comments.ts
@@ -1,11 +1,35 @@
 import { http, HttpResponse } from 'msw';
 import { toPlainObject } from 'lodash';
 
-import { IDeleteCommentResponse, IGetCommentResponse, IMinComment } from '../../src/interfaces';
+import {
+  IDeleteCommentResponse,
+  IGetCommentResponse,
+  IMinComment,
+  IUpdateCommentRequest,
+  IUpdateCommentResponse,
+} from '../../src/interfaces';
 import { baseURL } from '../../src/util';
 import { createMinComment } from '../mocks/dbActions';
 
 export const commentHandlers = [
+  http.patch(`${baseURL}/comments/:commentId`, async ({ request }) => {
+    const body = (await request.json()) as IUpdateCommentRequest;
+
+    if (!body.userId || !body.content) {
+      return HttpResponse.json(
+        {
+          message: 'Missing userId or commentId',
+        },
+        { status: 400 }
+      );
+    }
+    console.log('RUn');
+
+    return HttpResponse.json<IUpdateCommentResponse>({
+      message: 'success',
+    });
+  }),
+
   http.get(`${baseURL}/comments/:commentId`, ({ params }) => {
     if (!params.commentId) {
       return HttpResponse.json(
