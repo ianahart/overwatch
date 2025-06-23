3e1101a6d438dd5ded53acc606ec8b06a122f40c
Ian Hart <ianhart7790@gmail.com>
2025-06-23 19:11:57 -0400

    test: add a test suite for the Sidebar component inside the Teams package

---
+++ frontend/tests/components/Teams/Sidebar.test.tsx
@@ -0,0 +1,21 @@
+import { screen, render } from '@testing-library/react';
+
+import Sidebar from '../../../src/components/Teams/Sidebar';
+import { AllProviders } from '../../AllProviders';
+
+describe('Sidebar', () => {
+  const renderComponent = () => {
+    render(<Sidebar />, { wrapper: AllProviders });
+
+    return {
+      getHeading: () => screen.getByRole('heading', { name: /your teams/i }),
+    };
+  };
+
+    it('should render the heading', () => {
+
+        const {getHeading} = renderComponent()
+
+        expect(getHeading()).toBeInTheDocument()
+    });
+});
