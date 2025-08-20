3a4c1aeee6820c7f80bfb37136a3b9170262ba3b
Ian Hart <ianhart7790@gmail.com>
2025-08-20 19:15:59 -0400

    test: add a test suite for the NumberTextInputField component

--- frontend/tests/handlers/customFields.ts
+++ frontend/tests/handlers/customFields.ts
@@ -1,10 +1,19 @@
 import { http, HttpResponse } from 'msw';
 import { toPlainObject } from 'lodash';
-import { ICustomField, IFetchCustomFieldsResponse } from '../../src/interfaces';
+import { ICreateCustomFieldResponse, ICustomField, IFetchCustomFieldsResponse } from '../../src/interfaces';
 import { baseURL } from '../../src/util';
 import { db } from '../mocks/db';
 
 export const customFieldsHandlers = [
+  http.post(`${baseURL}/custom-fields`, () => {
+    return HttpResponse.json<ICreateCustomFieldResponse>(
+      {
+        message: 'success',
+      },
+      { status: 201 }
+    );
+  }),
+
   http.get(`${baseURL}/todo-cards/:todoCardId/custom-fields`, () => {
     const data: ICustomField[] = Array.from({ length: 3 }).map(() => {
       const customField: ICustomField = {
